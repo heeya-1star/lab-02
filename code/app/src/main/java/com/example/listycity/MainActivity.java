d4e29fcc78d2c51e6b74c72af9a6ca610ec0f674
heeya-1star <heeya1@ualberta.ca>
2026-01-16 01:04:46 -0700

    Add works

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.listycity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button adding;
     Button delete;
-
+    // indext to delete
+    int to_delete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         cityList = findViewById(R.id.city_list);
-
-        String []cities = {}; // fill with current
+        to_delete = -1;
+        String []cities = {"Edmonton"}; // fill with current
         dataList = new ArrayList<>();
         // add cities to dataList
         dataList.addAll(Arrays.asList(cities));
@@ -48,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         cityList.setAdapter(cityAdapter); // adapter transfer to cityList ( a listview)
 
         adding = findViewById(R.id.Add);
-
+        delete = findViewById(R.id.Delete);
         adding.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,8 +58,36 @@ public class MainActivity extends AppCompatActivity {
             }
         } );
 
+        setUpCityListListener();
+
+        delete.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+               delete_city(view, to_delete);
+            }
+
+        });
 
         }
+
+    private void setUpCityListListener() {
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                to_delete = position;
+                System.out.print("Rain");
+            }
+        });
+    }
+
+    private void delete_city(View view, int pos) {
+        if(pos>0 && pos < dataList.size()){
+            dataList.remove(pos);
+            // return to unusable state
+            to_delete = -1;
+        }
+    }
+
 
     private void addCity(View view) {
         EditText city = findViewById(R.id.editText);
